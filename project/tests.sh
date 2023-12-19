c2f07d923957f1513f660d4fa249711c2e75242b
Christoph Jung <christoph.jung@fau.de>
2023-12-19 17:41:17 +0100

    fixed path bug

--- project/tests.sh
+++ project/tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 python3 main/project/main.py
-path_to_db="./data/main_db.sqlite"
+path_to_db="main/data/main_db.sqlite"
 
 if [ -f "$path_to_db" ]; then
     echo "Output file was created successfully. Further tests are started now..."
-    python3 -m data.test_pipeline
+    python3 -m main.data.test_pipeline
 else
     echo "Outputfile couldn't be created."
 fi
