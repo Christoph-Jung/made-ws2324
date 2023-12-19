f3713f747d3e9831db3aa0a9a3a790c3f2854652
Christoph Jung <christoph.jung@fau.de>
2023-12-19 17:50:08 +0100

    test new paths

--- project/tests.sh
+++ project/tests.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-python3 main/project/main.py
-path_to_db="main/data/main_db.sqlite"
+python3 project/main.py
+path_to_db="data/main_db.sqlite"
 echo "ls"
 ls
-echo "ls main"
-ls main
-echo "ls main/data"
-ls main/data
+echo "ls data"
+ls data
 
 if [ -f "$path_to_db" ]; then
     echo "Output file was created successfully. Further tests are started now..."
-    python3 -m main.data.test_pipeline
+    python3 -m data.test_pipeline
 else
     echo "Outputfile couldn't be created."
 fi
