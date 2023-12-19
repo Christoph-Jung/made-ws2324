2166cad399ea90aa5a90c0a3a8a5555e62325a45
Christoph Jung <christoph.jung@fau.de>
2023-12-20 00:35:05 +0100

    removed unneccessary ls

--- project/tests.sh
+++ project/tests.sh
@@ -2,10 +2,6 @@
 
 python3 project/main.py
 path_to_db="data/main_db.sqlite"
-echo "ls"
-ls
-echo "ls data"
-ls data
 
 if [ -f "$path_to_db" ]; then
     echo "Output file was created successfully. Further tests are started now..."
